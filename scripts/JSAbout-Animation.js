365140b2f180cc426bdd49834ddcc71d62c63b7c
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2019-01-02 22:42:31 +0700

    init

---
+++ scripts/JSAbout-Animation.js
@@ -0,0 +1,13 @@
+
+  
+  /*------------- Animation About Me -----------------*/
+
+  var animation = bodymovin.loadAnimation({
+  container: document.getElementById('about-me-animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'aboutme.json'
+})
+
+ 
---
+++ scripts/JSswim-animate.js
@@ -0,0 +1,7 @@
+ var animation = bodymovin.loadAnimation({
+  container: document.getElementById('swim-animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'swim-animation.json'
+})
