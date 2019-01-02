27be4f5bed8177d891d936c97bd112344663e427
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2019-01-02 23:07:07 +0700

    remove docas

--- docs/scripts/JSswim-animate.js
+++
@@ -1,7 +0,0 @@
- var animation = bodymovin.loadAnimation({
-  container: document.getElementById('swim-animation'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'swim-animation.json'
-})
