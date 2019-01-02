9835810794a7df8409515746c5ca450333023e74
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2019-01-02 23:03:46 +0700

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+# nhungmy.github.io
---
+++ scripts/JSLogo-animation.js
@@ -0,0 +1,9 @@
+
+
+  var animation = bodymovin.loadAnimation({
+  container: document.getElementById('ContainerLogo'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'Logo-animation.json'
+})
